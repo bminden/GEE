89a8fff49ba540a5cd1c0fe941fb2d179dfe1c52
Julian Peffer <jepeffer@unomaha.edu>
2020-03-04 16:29:58 -0600

    I added user verification

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+  showText(text:string, password:string)
+  {
+    this.apiService.getUsers(text, password).subscribe((data)=>{
+     console.log(data);
+     if (data["data"] === 0)
+     {
+      alert("Bad account information");
+     } 
+     else{
+      alert("Verified");
+     }
+    });
+  }
 
   ngOnInit() {
   }
